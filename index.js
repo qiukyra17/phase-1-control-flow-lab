478a1802dd90b159ac93073b388059f8e3212de2
kyra qiu <kyraqiu@gmail.com>
2022-03-07 22:23:31 -0500

    commit tree ce8796d8ea12cff136de774142f4ba8e64372c95

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(numberFeet){
+    // Write your code here!
+    let scuberGreetingForFeet;
+    if (numberFeet<=400) {
+      scuberGreetingForFeet = `This one is on me!`;
+    } else if (numberFeet>2000) {
+      scuberGreetingForFeet = `I will gladly take your thirty bucks.`;
+    } if (numberFeet>2500) {
+      scuberGreetingForFeet = `No can do.`;
+    }
+    return scuberGreetingForFeet 
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  // return city==="NYC"?"Ok, sounds good.":"No go."
+let ternaryCheckCity;
+ternaryCheckCity = city==="NYC"?"Ok, sounds good.":"No go.";
+return ternaryCheckCity
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case `generous`: return `Thank you so much.`;
+    case `not as generous` : return `Thank you.`;
+    default: return `Bye.`;
+  }
 }
